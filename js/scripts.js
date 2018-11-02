f1a4dcb84ffb5c094492c67c897386ca2713100f
Alex Garcia <g.nightster11@gmail.com>
2018-11-02 08:34:26 -0700

    start outline of suto code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 function Pizza () {
-  // collect data for in coming order
+  //name of order
+  // what pizza size is
+  //what sizes did the cust order
+  //topping
+ this.total = null;
+}
+
+Pizza.prototype.size = function (){
+  // what sizes cost what amount
+}
+
+Pizza.prototype.total = function () {
+  // calculate topping amount
+
+}
+
+Pizza.prototype.topping = function (){
+  //what toppings cost
+}
+function Order(size,topping,name){
+  this.size = size;
+  this.topping= topping;
+  this.name = name
 }
