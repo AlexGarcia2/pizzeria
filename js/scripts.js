0ef43f60206b9ed727b7c8cd6614c1e3b4cf5ca0
Alex Garcia <g.nightster11@gmail.com>
2018-11-02 09:52:13 -0700

    add backend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 function Pizza (size,topping,name) {
-  this.size = size;
+  this.sizeCrust = size;
   this.topping = topping;
   this.name = name;
- this.total = null;
+ this.total = null
 }
 
-Pizza.prototype.size = function (){
+Pizza.prototype.sizeCrust = function (size){
   this.large = 15.00
+  this.medium = 12.00
+  this.small = 10.00
   // what sizes cost what amount
 }
 
 Pizza.prototype.total = function () {
-  total = this.total
-  total = (this.size + this.pepperoni)
-  
+  total = this.total();
+  total = (this.size + this.pepperoni);
+  return true;
 
 }
 
 Pizza.prototype.topping = function (){
+
   this.pepperoni = 3.00
   //what toppings cost
 }
