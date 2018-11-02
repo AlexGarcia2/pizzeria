c503140f32f090e5b32b31cc719884d09f38b7b5
Alex Garcia <g.nightster11@gmail.com>
2018-11-02 11:28:40 -0700

    working on business logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,36 +3,32 @@ function Pizza (size,top,name) {
   this.size = size;
   this.top = top;
   this.name = name;
- this.total = 0
+  this.total = 0
 }
-// Pizza.prototype.addOrder = function (order){
-//   Pizza.push(order)
-// }
-console.log(Pizza)
 
-Pizza.prototype.sizeCrust = function (){
+Pizza.prototype.sizeCrust = function() {
   debugger;
+  //for each loop
   var sizes = ["large"];
   var crust = this.size;
-  if (crust === sizes[i]){
-
+  if (crust === sizes){
+    return 15.00
   }
-
-
 }
 
-Pizza.prototype.topping = function (){
-  if (this.topping ==="pepperoni")
-  return false;
+Pizza.prototype.topping = function() {
+  if (this.top ==="pepperoni")
+  return 3.00;
   // this.pepperoni = "3.00";
   //what toppings cost
 }
 
-Pizza.prototype.totalAmount = function () {
-  total = this.total;
-  total = (this.size + this.pepperoni);
-  return total;
-  console.log("total")
+Pizza.prototype.totalAmount = function() {
+  var topping = this.topping();
+  var crust = this.sizeCrust();
+  total = (topping + crust);
+  this.total.push(total);
+  console.log(total)
 
 }
 
@@ -40,6 +36,7 @@ var order = new Pizza("large","pepperoni","alex")
 order.sizeCrust();
 order.topping();
 order.totalAmount();
+
 
 // function Order(size,topping,name){
 //   debugger;
